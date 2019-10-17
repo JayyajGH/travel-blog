cdcc1d022308864a7300c19a42ed501bf91975ce
Jason Harris <jason.harris@endsleigh.co.uk>
2019-10-17 16:10:43 +0100

    Style the header

--- src/components/header.js
+++ src/components/header.js
@@ -4,21 +4,24 @@ import { Link } from "gatsby"
 const Header = () => {
   return (
     <div className="pt-5 mb-8 flex flex-col items-center md:items-baseline md:flex-row justify-between">
-      <Link to="/">Logo</Link>
+      <Link to="/">
+        <h1 className="mt-1 text-red-800">Travel Blog</h1>
+        <p className="text-gray-700">View the world</p>
+      </Link>
       <nav className="flex">
         <ul className="flex list-none">
           <li>
-            <Link to="/" className="p-3">
+            <Link to="/" className="text-lg p-3 text-gray-700 hover:text-black">
               Home
             </Link>
           </li>
           <li>
-            <Link to="/blog" className="p-3">
+            <Link to="/blog" className="text-lg p-3 text-gray-700 hover:text-black">
               Blog
             </Link>
           </li>
           <li>
-            <Link to="/about" className="p-3">
+            <Link to="/about" className="text-lg p-3 text-gray-700 hover:text-black">
               About
             </Link>
           </li>
